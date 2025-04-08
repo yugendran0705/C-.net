caba5304a86c53283e2c04d5857e553db6ce4c83
Yugendran Sunder <yugendran.sunder@gmail.com>
2025-04-08 12:48:57 +0530

    Implemented Factorial

---
+++ MyFirstApp/Program.cs
@@ -0,0 +1,9 @@
+﻿static int Factorial(int n)
+{
+    if (n <= 1)
+        return 1;
+    return n * Factorial(n - 1);
+}
+Console.WriteLine("Enter a number to calculate its factorial:");
+int number = int.Parse(Console.ReadLine());
+Console.WriteLine($"Factorial of {number} is {Factorial(number)}");
